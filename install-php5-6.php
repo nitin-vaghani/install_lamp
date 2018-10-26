538964f9fc3ad82d68f8711353bd080d2bec43fe
Nitin Vaghani <nitinvaghani34@gmail.com>
2018-10-26 11:02:59 +0530

    Update install-php5-6.php

--- install-php5-6.php
+++ install-php5-6.php
@@ -8,3 +8,79 @@ sudo apt-get install -y php5.6
 sudo apt-get install php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
 
 sudo php -v
+
+# Install PHPMyAdmin
+sudo apt-get install phpmyadmin
+
+
+# Set group to www-data
+sudo chgrp www-data /var/www/html
+
+# Make it writable for the group
+sudo chmod 775 /var/www/html
+
+# Set GID to www-data for all sub-folders
+sudo chmod g+s /var/www/html
+
+# Add your username to www-data group
+sudo usermod -a -G www-data username
+
+# Finally change ownership to username
+sudo chown username /var/www/html
+
+# Your account shouldn't have any more permission issues
+
+Install Curl
+sudo apt-get install php5-curl
+
+Install Mycrypt
+sudo apt-get install php5-mcrypt
+
+Activate Mcrypt
+# Enable extension
+sudo php5enmod mcrypt
+
+# Restart Apache
+sudo service apache2 reload
+
+
+Enable Mod-Rewrite
+# enable rewrite
+sudo a2enmod rewrite
+
+# restart apache
+sudo service apache2 restart
+
+Install Composer First
+cd ~
+curl -sS https://getcomposer.org/installer | sudo php
+
+sudo mv composer.phar /usr/local/bin/composer
+
+Install Laravel
+# your-project is your destination folder
+cd /var/www/
+composer create-project laravel/laravel your-project --prefer-dist
+
+Set up your Apache virtual host
+
+# Copy default Apache conf
+sudo cp /etc/apache2/sites-available/000-default /etc/apache2/sites-available/laravel.conf
+# Edit laravel.conf and change DocumentRoot to /var/www/laravel/public
+sudo nano /etc/apache2/sites-available/laravel.conf
+# Edit laravel.conf add the following and save.
+DocumentRoot /var/www/laravel/public
+<Directory /var/www/laravel/public>
+ Options Indexes FollowSymLinks MultiViews
+ AllowOverride All
+ Order allow,deny
+ allow from all
+</Directory>
+# Reload Apache
+sudo service apache2 reload
+# Disable default Apache conf
+sudo a2dissite 000-default.conf
+# Enable laravel.conf
+sudo a2ensite laravel.conf
+# Reload Apache
+sudo service apache reload
