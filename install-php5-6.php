55cc9103f2431bcae6c6fe984aec50f7fadb0335
Nitin Vaghani <nitinvaghani34@gmail.com>
2018-10-26 11:01:08 +0530

    How to install php 5.6 in fresh ubuntu - Nitin Vaghani
    How to install php 5.6 in fresh ubuntu - Nitin Vaghani

---
+++ install-php5-6.php
@@ -0,0 +1,10 @@
+Use the following set of commands to enable PPA for PHP 5.6 in your Ubuntu system and install PHP 5.6.
+
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y php5.6
+
+sudo apt-get install php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
+
+sudo php -v
