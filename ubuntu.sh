5ba99806cdbe89efaf88936a054ead752a402590
Nitin Vaghani <nitinvaghani34@gmail.com>
2021-11-29 11:26:22 +0530

    Update ubuntu.sh
    php 5.6 new packages added

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,7 +2,7 @@ sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install -y php5.6
-sudo apt-get install php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
+sudo apt-get install php5.6-mcrypt php5.6-mbstring php5.6-curl php5.6-cli php5.6-mysql php5.6-gd php5.6-intl php5.6-xsl php5.6-zip php5.6-xml
 sudo apt install mysql-server
 sudo apt-get install phpmyadmin
 
