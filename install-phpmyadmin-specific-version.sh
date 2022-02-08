f95777cb3fbd9345722954c69ef825ba83ddc62c
Nitin Vaghani <nitinvaghani34@gmail.com>
2022-02-08 16:51:47 +0530

    Update install-phpmyadmin-specific-version.sh

--- install-phpmyadmin-specific-version.sh
+++ install-phpmyadmin-specific-version.sh
@@ -3,3 +3,7 @@ https://www.phpmyadmin.net/downloads/
 https://computingforgeeks.com/how-to-install-latest-phpmyadmin-on-ubuntu-debian/
 
 sudo mysql -u root
+
+mysql> USE mysql;
+
+mysql> UPDATE mysql.user SET authentication_string=PASSWORD(''), plugin='mysql_native_password' WHERE User='root' AND Host='localhost';EXIT;
